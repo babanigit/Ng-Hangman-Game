962a30e779239e7fb2cb7c30d6892e396a4e21f3
babanigit <Aniketgithub07>
2024-07-21 23:19:52 +0530

    some changes

--- src/app/components/hangman-display/hangman-display.component.ts
+++ src/app/components/hangman-display/hangman-display.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hangman-display',
   templateUrl: './hangman-display.component.html',
   styleUrl: './hangman-display.component.scss'
 })
-export class HangmanDisplayComponent {
+export class HangmanDisplayComponent implements OnInit {
+  
+  @Input() guesses:string[]=[];
+  @Input() question:string= "";
+  MAX_MISTAKES=7;
+  mistakesRemaining:number;
+
+  constructor(){
+    this.mistakesRemaining=this.MAX_MISTAKES;
+  }
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
 
 }
--- src/app/components/hangman/hangman.component.ts
+++ src/app/components/hangman/hangman.component.ts
@@ -1,31 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { HangmanService } from '../../services/hangman.service';
 
 @Component({
   selector: 'app-hangman',
   templateUrl: './hangman.component.html',
-  styleUrl: './hangman.component.scss'
+  styleUrl: './hangman.component.scss',
 })
-export class HangmanComponent implements OnInit {
+export class HangmanComponent implements OnInit, OnChanges {
+  question: string = '';
+  questions: string[] = [];
+  guesses: string[] = [];
+  category: string = '';
 
-  question:string= '';
-  questions:string[]= [];
-  guesses:string[] =[];
-  category:string='';
+  constructor(private hangmanService: HangmanService) {}
 
-  constructor(private hangmanService:HangmanService) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes['guesses'].currentValue &&
+      changes['guesses'].currentValue.length &&
+      changes['guesses'].currentValue != changes['guesses'].previousValue
+    ) {
+      console.log(changes['guesses'].currentValue)
+    }
+  }
 
   ngOnInit(): void {
-    this.hangmanService.getQuestions().subscribe((res)=> {
-      this.questions= res.items;
-      this.category=res.category;
+    this.hangmanService.getQuestions().subscribe((res) => {
+      this.questions = res.items;
+      this.category = res.category;
       this.pickNewQuestions();
-    })
+    });
+  }
+
+  //push the key to guesses array
+  guess(letter: string) {
+    if (!letter || this.guesses.includes(letter)) {
+      return;
+    }
+    // this.guesses.push(letter);
+    this.guesses= [...this.guesses, letter];
+  }
+
+  dummyClick() {
+    let key = prompt('enter the key') || '';
+    this.guess(key);
+  }
+
+  reset() {
+    this.guesses = [];
+    this.pickNewQuestions();
   }
 
   pickNewQuestions() {
-    console.log("data fetched")
-    // console.log( "this is quesitons ", this.questions)
+    const randomIndex = Math.floor(Math.random() * this.questions.length); //to get random index number
+    this.question = this.questions[randomIndex];
+    console.log(this.question);
   }
-
 }
